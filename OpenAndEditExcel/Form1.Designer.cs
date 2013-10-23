43bb7b881d52ed2e59edbc0c63a814d1700b5d3f
koletzosck <koletzosxri@live.com>
2013-10-23 15:02:58 +0300

    First commit

---
+++ OpenAndEditExcel/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace OpenAndEditExcel
+{
+    partial class Mainfrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainOpenfile = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MainOpenfile
+            // 
+            this.MainOpenfile.FileName = "MainOpenfile";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Mainfrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 291);
+            this.Controls.Add(this.button1);
+            this.Name = "Mainfrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog MainOpenfile;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ OpenAndEditExcel/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel; 
+
+namespace OpenAndEditExcel
+{
+    public partial class Mainfrm : Form
+    {
+        public Mainfrm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MainFunction();
+        }
+
+        private void MainFunction()
+        {
+            Excel.Application excelApp = null;
+            try
+            {
+                excelApp = new Excel.Application();
+                EditCells(OpenFile(excelApp));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (excelApp != null)
+                {
+                    excelApp.Quit();
+                }
+            }
+        }
+
+        private Excel.Worksheet OpenFile(Excel.Application excelApp)
+        {
+            MainOpenfile.FileName = "";
+            MainOpenfile.ShowDialog();
+            string fileName;
+            if (MainOpenfile.FileName == "")
+            {
+                throw new Exception("a filename is required!");
+            }
+
+            fileName = MainOpenfile.FileName;
+
+            Excel.Workbook excelWorkbook = excelApp.Workbooks.Open(fileName, 0, false, 5, "", "", false, Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false);
+            Excel.Sheets excelSheets = excelWorkbook.Worksheets;
+
+            if (excelSheets.Count == 0)
+            {
+                throw new Exception("At least one sheet is required !");
+            }
+            Excel.Worksheet excelWorksheet = (Excel.Worksheet)excelSheets[1];
+
+            return excelWorksheet;
+        }
+
+        private void EditCells(Excel.Worksheet excelWorksheet)
+        {
+            var col = excelWorksheet.UsedRange.Columns["H:H"];
+            Random rnd = new Random();
+            string tempsubstr = String.Empty;
+            string substr = String.Empty;
+            foreach (Excel.Range item in col.Cells)
+            {
+                tempsubstr = item.Value.ToString();
+                if (item.Value != "CarPlate")
+                {
+                    switch (tempsubstr.Length)
+                    {
+                        case 6 :
+                            substr=item.Value.Substring(0, 2);
+                            break;
+                        case 7 :
+                            substr=item.Value.Substring(0, 3);
+                            break;
+                        default :
+                            substr = item.Value.Substring(0, 2);
+                            break;
+                    }
+
+                    int newNumber = rnd.Next(1, 9999);
+                    item.Value = substr + newNumber.ToString().PadLeft(4, '0');
+                }
+            } 
+        }
+    }
+}
